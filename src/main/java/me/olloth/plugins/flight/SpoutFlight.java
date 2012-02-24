7781f456be738e6745d1ba0f60b8e7e1ddaf2f1b
Olloth <olloth@gmail.com>
2012-02-24 17:10:53 -0500

    Updated to the new event system

--- src/main/java/me/olloth/plugins/flight/SpoutFlight.java
+++ src/main/java/me/olloth/plugins/flight/SpoutFlight.java
@@ -24,18 +24,12 @@ import java.util.logging.Logger;
 import me.olloth.plugins.flight.keys.MovementKey;
 import me.olloth.plugins.flight.keys.ToggleKey;
 import me.olloth.plugins.flight.keys.UpDownKey;
-import me.olloth.plugins.flight.listener.Entities;
 import me.olloth.plugins.flight.listener.Players;
-import me.olloth.plugins.flight.listener.Spouts;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
-
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.keyboard.KeyBindingManager;
 import org.getspout.spoutapi.keyboard.Keyboard;
@@ -43,194 +37,180 @@ import org.getspout.spoutapi.plugin.SpoutPlugin;
 
 public class SpoutFlight extends SpoutPlugin {
 
-    public static final Logger log = Logger.getLogger("Minecraft");
-    public static String PREFIX = "[SpoutFlight] ";
+	public static final Logger log = Logger.getLogger("Minecraft");
+	public static String PREFIX = "[SpoutFlight] ";
 
-    private PluginDescriptionFile info;
-    private PluginManager pm;
+	private PluginDescriptionFile info;
 
-    private Players players;
-    private Spouts spouts;
-    private Entities entities;
+	public Players players;
 
-    private Config config;
+	private Config config;
 
-    final private Map<String, Boolean> enabled = new HashMap<String, Boolean>();
-    final private Map<String, Double> gravitys = new HashMap<String, Double>();
-    final private Map<String, Integer> speeds = new HashMap<String, Integer>();
+	final private Map<String, Boolean> enabled = new HashMap<String, Boolean>();
+	final private Map<String, Double> gravitys = new HashMap<String, Double>();
+	final private Map<String, Integer> speeds = new HashMap<String, Integer>();
 
-    public void onDisable() {
-        config.saveMaps();
-        log.log(Level.INFO, PREFIX + "is now disabled.");
-    }
-    
-    public void onEnable() {
-	info = getDescription();
-	pm = getServer().getPluginManager();
-
-	config = new Config(this);
-
-	for (Player player : getServer().getOnlinePlayers()) {
-            enabled.put(player.getName(), false);
-            speeds.put(player.getName(), config.getDefaultSpeed());
+	public void onDisable() {
+		config.saveMaps();
+		log.log(Level.INFO, PREFIX + "is now disabled.");
 	}
 
-	players = new Players(this);
-	pm.registerEvent(Type.PLAYER_TELEPORT, players, Priority.Monitor, this);
-	pm.registerEvent(Type.PLAYER_BED_ENTER, players, Priority.Low, this);
-	pm.registerEvent(Type.PLAYER_PORTAL, players, Priority.Low, this);
-	pm.registerEvent(Type.PLAYER_RESPAWN, players, Priority.Low, this);
+	public void onEnable() {
+		info = getDescription();
 
-	entities = new Entities(this);
-	pm.registerEvent(Type.ENTITY_DAMAGE, entities, Priority.Low, this);
+		config = new Config(this);
 
-	spouts = new Spouts(this);
-	pm.registerEvent(Type.CUSTOM_EVENT, spouts, Priority.Low, this);
-		
-	KeyBindingManager kbm = SpoutManager.getKeyBindingManager();
-                
-	try {
-            kbm.registerBinding("SpoutFlight.Toggle", Keyboard.KEY_LCONTROL, "The key to toggle flying on/off", new ToggleKey(this), this);
-            kbm.registerBinding("SpoutFlight.Up", Keyboard.KEY_SPACE, "The key to fly up", new UpDownKey(this, -0.1F), this);
-            kbm.registerBinding("SpoutFlight.Down", Keyboard.KEY_LSHIFT, "The key to fly down", new UpDownKey(this, 0.1F), this);
-            kbm.registerBinding("SpoutFlight.Forward", Keyboard.KEY_W, "The key to move forward", new MovementKey(this), this);
-            kbm.registerBinding("SpoutFlight.Backward", Keyboard.KEY_S, "The key to move backward", new MovementKey(this), this);
-            kbm.registerBinding("SpoutFlight.Left", Keyboard.KEY_A, "The key to move left", new MovementKey(this), this);
-            kbm.registerBinding("SpoutFlight.Right", Keyboard.KEY_D, "The key to move right", new MovementKey(this), this);
+		for (Player player : getServer().getOnlinePlayers()) {
+			enabled.put(player.getName(), false);
+			speeds.put(player.getName(), config.getDefaultSpeed());
+		}
 
-            } catch (Exception e) {
-		e.printStackTrace();
-            }
-                
-            log.log(Level.INFO, PREFIX + "version " + info.getVersion() + " is now enabled.");
-    }
+		players = new Players(this);
 
-    /**
-     * Calls when a player uses a command.
-     */
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        String commandName = command.getName().toLowerCase();
-        boolean commandSuccess = false;
+		KeyBindingManager kbm = SpoutManager.getKeyBindingManager();
 
-        // Only Players
-	if (!(sender instanceof Player)) {
-            sender.sendMessage(PREFIX + "/" + commandName + " can only be run from in game.");
-            commandSuccess = true;
+		try {
+			kbm.registerBinding("SpoutFlight.Toggle", Keyboard.KEY_LCONTROL, "The key to toggle flying on/off", new ToggleKey(this), this);
+			kbm.registerBinding("SpoutFlight.Up", Keyboard.KEY_SPACE, "The key to fly up", new UpDownKey(this, -0.1F), this);
+			kbm.registerBinding("SpoutFlight.Down", Keyboard.KEY_LSHIFT, "The key to fly down", new UpDownKey(this, 0.1F), this);
+			kbm.registerBinding("SpoutFlight.Forward", Keyboard.KEY_W, "The key to move forward", new MovementKey(this), this);
+			kbm.registerBinding("SpoutFlight.Backward", Keyboard.KEY_S, "The key to move backward", new MovementKey(this), this);
+			kbm.registerBinding("SpoutFlight.Left", Keyboard.KEY_A, "The key to move left", new MovementKey(this), this);
+			kbm.registerBinding("SpoutFlight.Right", Keyboard.KEY_D, "The key to move right", new MovementKey(this), this);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		log.log(Level.INFO, PREFIX + "version " + info.getVersion() + " is now enabled.");
 	}
 
-	else if (commandName.equals("sfspeed")) {
-            Player player = (Player) sender;
+	/**
+	 * Calls when a player uses a command.
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		String commandName = command.getName().toLowerCase();
+		boolean commandSuccess = false;
 
-            if (args.length == 0) {
-                player.sendMessage("Your current speed is " + getPlayerSpeed(player));
-		commandSuccess = true;
+		// Only Players
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(PREFIX + "/" + commandName + " can only be run from in game.");
+			commandSuccess = true;
+		}
 
-            } else {
+		else if (commandName.equals("sfspeed")) {
+			Player player = (Player) sender;
 
-                try {
-                    int speed = Integer.parseInt(args[0]);
-                    
-                    if (speed < 1) {
-                        speed = 1;
-                    } else if (speed > config.getMaxSpeed()) {
-                        speed = config.getMaxSpeed();
-                    }
-                    
-                    setPlayerSpeed(player, speed);
-                    player.sendMessage("Your current speed is " + speed);
+			if (args.length == 0) {
+				player.sendMessage("Your current speed is " + getPlayerSpeed(player));
+				commandSuccess = true;
 
-                    commandSuccess = true;
-                } catch (NumberFormatException e) {
-                    commandSuccess = false;
-                }
+			} else {
 
-            }
-        }
+				try {
+					int speed = Integer.parseInt(args[0]);
 
-        return commandSuccess;
-    }
+					if (speed < 1) {
+						speed = 1;
+					} else if (speed > config.getMaxSpeed()) {
+						speed = config.getMaxSpeed();
+					}
 
-    public void setPlayerEnabled(Player player, boolean enable) {
-        if (enabled == null) {
-            log.severe("SpoutFlight: setPlayerEnabled, enabled map is null");
-            return; 
-        }
-        
-        if (player == null) {
-            log.warning("SpoutFlight: setPlayerEnabled, player is null");
-            return;
-	}
-		
-        enabled.put(player.getName(), enable);
-	
-    }
+					setPlayerSpeed(player, speed);
+					player.sendMessage("Your current speed is " + speed);
 
-    public boolean getPlayerEnabled(Player player) {
-        if (enabled == null) {
-            log.severe("SpoutFlight: getPlayerEnabled, enabled map is null");
-            return false; 
-        }
-	
-        if (player == null) {
-            log.warning("SpoutFlight: getPlayerEnabled, player is null");
-            return false;
-	}
-		
-	if (!enabled.containsKey(player.getName())) {
-            setPlayerEnabled(player, false);
-        }
-        
-        return enabled.get(player.getName());
-    }
+					commandSuccess = true;
+				} catch (NumberFormatException e) {
+					commandSuccess = false;
+				}
 
-    public void setPlayerSpeed(Player player, int speed) {
-        speeds.put(player.getName(), speed);
-	SpoutManager.getPlayer(player).setAirSpeedMultiplier(1 * speed);
-    }
+			}
+		}
 
-    public int getPlayerSpeed(Player player) {
-        if (!speeds.containsKey(player.getName())) {
-            setPlayerSpeed(player, config.getDefaultSpeed());
+		return commandSuccess;
 	}
 
-        return speeds.get(player.getName());
-    }
-	
-    public void setPlayerGravity(Player player, double gravity) {
-        gravitys.put(player.getName(), gravity);
-    }
-	
-    public double getPlayerGravity(Player player) {
-        if (!gravitys.containsKey(player.getName())) {
-            setPlayerGravity(player, config.getDefaultGravity());
-        }
+	public void setPlayerEnabled(Player player, boolean enable) {
+		if (enabled == null) {
+			log.severe("SpoutFlight: setPlayerEnabled, enabled map is null");
+			return;
+		}
 
-        return gravitys.get(player.getName());
-    }
+		if (player == null) {
+			log.warning("SpoutFlight: setPlayerEnabled, player is null");
+			return;
+		}
 
-    public void setEnabledMap(Map<String, Boolean> map) {
-        enabled.clear();
-	if (map != null )
-            enabled.putAll(map);
-    }
+		enabled.put(player.getName(), enable);
 
-    public Map<String, Boolean> getEnabledMap() {
-        return enabled;
-    }
-	
-    public void setGravityMap(Map<String, Double> map) {
-        gravitys.clear();
-        if(map != null)
-            gravitys.putAll(map);
-    }
-	
-    public Map<String, Double> getGravityMap() {
-        return gravitys;
-    }
+	}
 
-    public Config getSFConfig() {
-        return config;
-    }
-    
+	public boolean getPlayerEnabled(Player player) {
+		if (enabled == null) {
+			log.severe("SpoutFlight: getPlayerEnabled, enabled map is null");
+			return false;
+		}
+
+		if (player == null) {
+			log.warning("SpoutFlight: getPlayerEnabled, player is null");
+			return false;
+		}
+
+		if (!enabled.containsKey(player.getName())) {
+			setPlayerEnabled(player, false);
+		}
+
+		return enabled.get(player.getName());
+	}
+
+	public void setPlayerSpeed(Player player, int speed) {
+		speeds.put(player.getName(), speed);
+		SpoutManager.getPlayer(player).setAirSpeedMultiplier(1 * speed);
+	}
+
+	public int getPlayerSpeed(Player player) {
+		if (!speeds.containsKey(player.getName())) {
+			setPlayerSpeed(player, config.getDefaultSpeed());
+		}
+
+		return speeds.get(player.getName());
+	}
+
+	public void setPlayerGravity(Player player, double gravity) {
+		gravitys.put(player.getName(), gravity);
+	}
+
+	public double getPlayerGravity(Player player) {
+		if (!gravitys.containsKey(player.getName())) {
+			setPlayerGravity(player, config.getDefaultGravity());
+		}
+
+		return gravitys.get(player.getName());
+	}
+
+	public void setEnabledMap(Map<String, Boolean> map) {
+		enabled.clear();
+		if (map != null)
+			enabled.putAll(map);
+	}
+
+	public Map<String, Boolean> getEnabledMap() {
+		return enabled;
+	}
+
+	public void setGravityMap(Map<String, Double> map) {
+		gravitys.clear();
+		if (map != null)
+			gravitys.putAll(map);
+	}
+
+	public Map<String, Double> getGravityMap() {
+		return gravitys;
+	}
+
+	public Config getSFConfig() {
+		return config;
+	}
+
 }
--- src/main/java/me/olloth/plugins/flight/listener/Players.java
+++ src/main/java/me/olloth/plugins/flight/listener/Players.java
@@ -19,21 +19,28 @@ package me.olloth.plugins.flight.listener;
 
 import me.olloth.plugins.flight.SpoutFlight;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
 
-public class Players extends PlayerListener {
+public class Players implements Listener {
 	SpoutFlight plugin;
 
 	public Players(SpoutFlight plugin) {
 		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
-	@Override
+	@EventHandler
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
 		if (!event.getFrom().getWorld().getName().equals(event.getTo().getWorld().getName())) {
 			plugin.setPlayerEnabled(event.getPlayer(), false);
@@ -42,24 +49,48 @@ public class Players extends PlayerListener {
 		}
 	}
 
-	@Override
+	@EventHandler
 	public void onPlayerBedEnter(PlayerBedEnterEvent event) {
 		plugin.setPlayerEnabled(event.getPlayer(), false);
 		SpoutManager.getPlayer(event.getPlayer()).setGravityMultiplier(1);
 		SpoutManager.getPlayer(event.getPlayer()).setAirSpeedMultiplier(1);
 	}
 
-	@Override
+	@EventHandler
 	public void onPlayerPortal(PlayerPortalEvent event) {
 		plugin.setPlayerEnabled(event.getPlayer(), false);
 		SpoutManager.getPlayer(event.getPlayer()).setGravityMultiplier(1);
 		SpoutManager.getPlayer(event.getPlayer()).setAirSpeedMultiplier(1);
 	}
 
-	@Override
+	@EventHandler
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
 		plugin.setPlayerEnabled(event.getPlayer(), false);
 		SpoutManager.getPlayer(event.getPlayer()).setGravityMultiplier(1);
 		SpoutManager.getPlayer(event.getPlayer()).setAirSpeedMultiplier(1);
+	}
+
+	@EventHandler
+	public void onSpoutCraftEnable(SpoutCraftEnableEvent event) {
+		if (event.getPlayer() == null)
+			return;
+
+		if (plugin.getPlayerEnabled(event.getPlayer())) {
+			event.getPlayer().setCanFly(true);
+			event.getPlayer().setGravityMultiplier(0);
+			event.getPlayer().setAirSpeedMultiplier(1 * plugin.getPlayerSpeed(event.getPlayer()));
+		}
+
+	}
+
+	@EventHandler
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event.getCause().equals(DamageCause.FALL) && event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+
+			if (plugin.getPlayerEnabled(player) || player.hasPermission("spoutflight.nofalldmg")) {
+				event.setCancelled(true);
+			}
+		}
 	}
 }
